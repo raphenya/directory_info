a767098633987d73fd97953b4656ab174c3cd308
Amos Raphenya <raphenar@mcmaster.ca>
2016-06-09 12:32:35 -0400

    adds info script

---
+++ info.sh
@@ -0,0 +1,15 @@
+echo "\n"
+echo "FILE INFO SUMMARY"
+echo "------------------"
+du -sh *
+echo "\n\n"
+
+echo "FILE INFO DETAILS (Recent at bottom)"
+echo "------------------"
+ls -lhart
+echo "\n\n"
+
+echo "DISK INFO"
+echo "------------------"
+df -h
+echo "\n\n"
